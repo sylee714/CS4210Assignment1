32bed6dfa1f1e5ea0a7ed8380376c5f45fe232a0
Seungyun Lee <sylee714@gmail.com>
2021-09-08 22:52:10 -0700

    finished decision_tree.py

--- decision_tree.py
+++ decision_tree.py
@@ -16,6 +16,39 @@ db = []
 X = []
 Y = []
 
+age_values = {'Young' : 1, 'Presbyopic' : 2, 'Prepresbyopic' : 3}
+spectacle_pres_values = {'Myope' : 1, 'Hypermetrope' : 2}
+astigmatism_values = {'Yes' : 1, 'No' : 2}
+tear_prod_rate_values = {'Normal' : 1, 'Reduced' : 2}
+recommend_lenses_values = {'Yes' : 1, 'No' : 2}
+
+def transformEntry(entry):
+    new_entry = []
+
+    new_entry.append(age_values[entry[0]])
+    new_entry.append(spectacle_pres_values[entry[1]])
+    new_entry.append(astigmatism_values[entry[2]])
+    new_entry.append(tear_prod_rate_values[entry[3]])
+
+    return new_entry
+
+def transformData(db):
+    x = []
+
+    for entry in db:
+        x.append(transformEntry(entry))
+
+    return x
+
+def transformLabel(db):
+    y = []
+
+    for entry in db:
+        last_index = len(entry) - 1
+        y.append(recommend_lenses_values[entry[last_index]])
+
+    return y
+
 #reading the data in a csv file
 with open('contact_lens.csv', 'r') as csvfile:
   reader = csv.reader(csvfile)
@@ -24,13 +57,17 @@ with open('contact_lens.csv', 'r') as csvfile:
          db.append (row)
          print(row)
 
+print(db)
+
 #transform the original training features to numbers and add to the 4D array X. For instance Young = 1, Prepresbyopic = 2, Presbyopic = 3, so X = [[1, 1, 1, 1], [2, 2, 2, 2], ...]]
 #--> add your Python code here
-# X =
+X = transformData(db)
+print(X)
 
 #transform the original training classes to numbers and add to the vector Y. For instance Yes = 1, No = 2, so Y = [1, 1, 2, 2, ...]
-#--> addd your Python code here
-# Y =
+#--> add your Python code here
+Y = transformLabel(db)
+print(Y)
 
 #fitting the decision tree to the data
 clf = tree.DecisionTreeClassifier(criterion = 'entropy')
